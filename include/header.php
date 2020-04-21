f5583411edd6a555ce47aafdaf485a619bd088b4
Hunter <thecryptohunter@protonmail.com>
2020-04-21 15:15:30 +0100

    add social media snapshot

--- include/header.php
+++ include/header.php
@@ -16,9 +16,18 @@ if (isset($bannerAd)) {
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	<link rel="stylesheet" href="assets/css/main.css" />
 	<noscript>
-		<link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	<link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+	<meta property="og:title" content="Trustaking.com">
+	<meta property="og:description" content="The trusted home of cold staking.">
+	<meta property="og:image" content="images/thumbnail.png">
+	<meta property="og:image:width" content="1200">
+	<meta property="og:image:height" content="630">
+	<!--<meta property="og:url" content="">-->
+  	<meta name="twitter:card" content="summary_large_image">
 </head>
 
 <body class="landing is-preload">
