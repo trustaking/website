bf00db91b70c1acac306e4eeba43a615ed72aa56
Hunter <thecryptohunter@protonmail.com>
2020-11-13 12:39:54 +0000

    Add strax link

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 		<h2>TRUSTAKING.COM </h3>
 		<p>The trusted home <br> of cold staking</p>
 		<ul class="actions special">
+			<li><a href="http://strax.trustaking.com"><img src="images/coin_logo-strax.png" alt="STRAX" width="50" height="50"></a></li>
 			<li><a href="http://solaris.trustaking.com" class="image-link"><img src="images/coin_logo-solaris.png" alt="Solaris" width="50" height="50"></a></li>
 			<li><a href="http://x42.trustaking.com"><img src="images/coin_logo-x42.png" alt="X42" width="100" height="50"></a></li>
 			<li><a href="http://city.trustaking.com"><img src="images/coin_logo-city.png" alt="City" width="50" height="50"></a></li>
 			<li><a href="http://xds.trustaking.com"><img src="images/coin_logo-xds.png" alt="XDS" width="50" height="50"></a></li>
 			<li><a href="http://ams.trustaking.com"><img src="images/coin_logo-amsterdamcoin.png" alt="AMS" width="50" height="50"></a></li>
-			<li><a href="http://strax.trustaking.com"><img src="images/coin_logo-strax.png" alt="STRAX" width="50" height="50"></a></li>
 <!--
 			<li><a href="http://impleum.trustaking.com"><img src="images/coin_logo-impleum.png" alt="IMPL" width="50" height="50"></a></li>
 -->
